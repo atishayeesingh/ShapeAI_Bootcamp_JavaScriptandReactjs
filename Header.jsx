8d0458334f6babbb9b365aa5e139c323f1a39307
atishayeesingh <84901984+atishayeesingh@users.noreply.github.com>
2021-06-06 00:21:04 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(){
+ return (
+ <header>
+<h1> Atishayee Singh </h1>
+ </header>  
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Note(){
+ return (
+ <div className="note">
+<h1> About Me </h1>
+<p>
+  Hi! I am Atishayee. I am in my I year of engineering and here is my project for the ShapeAI Bootcamp.
+  The Bootcamp was really informative and fun.  </p>
+ </div>  
+  );
+}
+
+export default Note;
